7fdbc43ac4e727a94dc2a101461c96f96a077e1e
eroSilva <eronildo.silva@outlook.com>
2019-07-25 15:04:26 -0300

    Deploy no Github Pages

---
+++ precache-manifest.c522ce4f49fd667c80e7ef677346eb95.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "874cd7da9843aa7702c30f30a2f9d37d",
+    "url": "index.html"
+  },
+  {
+    "revision": "c73752e1c7151729f834",
+    "url": "app.js"
+  },
+  {
+    "revision": "c73752e1c7151729f834",
+    "url": "app.css"
+  },
+  {
+    "revision": "5fd57c2d4a4ee63394582a4df474a9a1",
+    "url": "./icon_512x512.png"
+  },
+  {
+    "revision": "3f002aaddf8ebc8072337e320ed8ea3b",
+    "url": "./icon_384x384.png"
+  },
+  {
+    "revision": "d1f7eaa82be08068a06a5f48f2a47ea6",
+    "url": "./icon_256x256.png"
+  }
+];
